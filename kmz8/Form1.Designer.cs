3ab9b33cbc2f1b1db68a8283fb663698525012f8
Valeriy <requiemnosoul@gmail.com>
2020-07-15 05:37:52 +0500

    Initial commit

---
+++ kmz8/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace kmz8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(9, 10);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(259, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(9, 84);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(258, 19);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Atkin";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(9, 107);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(258, 254);
+            this.textBox2.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(9, 32);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(258, 19);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Test";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 54);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(10, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "-";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(278, 372);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Atkin; Test Millera-Rabina";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ kmz8/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kmz8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            int n = Convert.ToInt32(textBox1.Text);
+            textBox2.Text = Atkin(n);
+        }
+        string Atkin(int limit)
+        {
+            string res = "";
+            bool[] is_prime = new bool[limit+1];
+            int n;
+
+            int sqr_lim = (int)Math.Sqrt(limit);
+            for (int i = 0; i <= limit; i++) is_prime[i] = false;
+            is_prime[2] = true;
+            is_prime[3] = true;
+
+            int x2 = 0;
+            for (int i = 1; i <= sqr_lim; i++)
+            {
+                x2 += 2 * i - 1;
+                int y2 = 0;
+                for (int j = 1; j <= sqr_lim; j++)
+                {
+                    y2 += 2 * j - 1;
+
+                    n = 4 * x2 + y2;
+                    if ((n <= limit) && (n % 12 == 1 || n % 12 == 5))
+                        is_prime[n] = !is_prime[n];
+                    // n = 3 * x2 + y2; 
+                    n -= x2;
+                    if ((n <= limit) && (n % 12 == 7))
+                        is_prime[n] = !is_prime[n];
+                    // n = 3 * x2 - y2;
+                    n -= 2 * y2;
+                    if ((i > j) && (n <= limit) && (n % 12 == 11))
+                        is_prime[n] = !is_prime[n];
+                }
+            }
+            for (int i = 5; i <= sqr_lim; i++)
+            {
+                if (is_prime[i])
+                {
+                    n = i * i;
+                    for (int j = n; j <= limit; j += n)
+                    {
+                        is_prime[j] = false;
+                    }
+                }
+            }
+            for (int i = 2; i <= limit; i++)
+            {
+                if ((is_prime[i]))
+                {
+                    res += i+ " ";
+                }
+            }
+
+            return res;
+        }
+        bool MilRab(long n) // Тест Миллера-Рабина на простое число
+        {
+            Random rnd = new Random();
+            long a, s = 0;
+            long k = Convert.ToInt32(Math.Log(n, 2));
+            long t = n - 1;
+            while (t % 2 == 0)
+            {
+                t /= 2;
+                s++;
+            }
+            for (int i = 0; i < k; i++)
+            {
+                a = rnd.Next(2, (int)n - 1);
+                long x = (long)BigInteger.ModPow(a, t, n);
+                if (x == 1 || x == n - 1)
+                    continue;
+                for (int j = 0; j < s - 1; j++)
+                {
+                    x = (long)BigInteger.ModPow(x, 2, n);
+                    if (x == 1)
+                        return false;
+                    if (x == n - 1)
+                        break;
+                }
+                if (x != n - 1)
+                    return false;
+            }
+            return true;
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if (MilRab(Convert.ToInt64(textBox1.Text)) == true)
+                label1.Text = "Вероятностно простое";
+            else
+                label1.Text = "Составное";
+        }
+    }
+}
